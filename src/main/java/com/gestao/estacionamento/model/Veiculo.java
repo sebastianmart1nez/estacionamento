c42acf3356151a8276b29703b1168f04d9512502
InesFaleiroR <insrsd@gmail.com>
2026-04-15 17:33:34 +0100

    Model Veiculo - Opiniões?

--- src/main/java/com/gestao/estacionamento/model/Veiculo.java
+++ src/main/java/com/gestao/estacionamento/model/Veiculo.java
@@ -1,4 +1,20 @@
 package com.gestao.estacionamento.model;
 
 public class Veiculo {
+
+    private String matricula;
+    private String horaEntrada;
+
+    public Veiculo(String matricula, String horaEntrada) {
+        this.matricula = matricula;
+        this.horaEntrada = horaEntrada;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getHoraEntrada() {
+        return horaEntrada;
+    }
 }
