d8da1132c07b88557e3af7002e776970ed59f0e8
João_Patrício <8452350@formacao.iefp.pt>
2026-04-15 17:46:47 +0100

    Criação do controler

--- src/main/java/com/gestao/estacionamento/controler/VeiculoControler.java
+++ src/main/java/com/gestao/estacionamento/controler/VeiculoControler.java
@@ -1,4 +1,29 @@
 package com.gestao.estacionamento.controler;
 
+import org.springframework.ui.Model;
+import com.gestao.estacionamento.model.Veiculo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class VeiculoControler {
+
+    private List<Veiculo> veiculos = new ArrayList<>();
+
+@GetMapping("/veiculos")
+    public String listarVeiculos(Model model) {
+        model.addAttribute("mensagem", "Lista de Veículos");
+        model.addAttribute("lista", veiculos);
+        return "veiculo";
+    }
+@PostMapping("/veiculos")
+    public String adicionarVeiculo(@RequestParam String matricula, @RequestParam String horaEntrada) {
+        veiculos.add(new Veiculo(matricula, horaEntrada));
+        return "redirect:/veiculos";
+    }
 }
