560347a3429bbebeb82ee6f74d351d017d5253fb
João_Patrício <8452350@formacao.iefp.pt>
2026-04-23 16:35:09 +0100

    Alteração das dependencias e alteração do model.

--- src/main/java/com/gestao/estacionamento/model/Veiculo.java
+++ src/main/java/com/gestao/estacionamento/model/Veiculo.java
@@ -1,19 +1,23 @@
 package com.gestao.estacionamento.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Veiculo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String matricula;
     private String horaEntrada;
 
-    public Veiculo(String matricula, String horaEntrada) {
-        this.matricula = matricula;
-        this.horaEntrada = horaEntrada;
-    }
 
-    public String getMatricula() {
-        return matricula;
-    }
-
-
-    public String getHoraEntrada() { return horaEntrada; }
 }
