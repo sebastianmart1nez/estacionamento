74492aa6e24ccface3a4dbac6bd08b6bf23d448c
João_Patrício <8452350@formacao.iefp.pt>
2026-05-07 16:42:58 +0100

    Criação do Home Controller.

---
+++ src/main/java/com/gestao/estacionamento/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.gestao.estacionamento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";
+    }
+}
