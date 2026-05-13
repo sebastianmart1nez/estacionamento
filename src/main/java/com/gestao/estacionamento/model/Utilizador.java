d05b51d9abf0b044f047411eac88ddcde8b389d8
InesFaleiroR <insrsd@gmail.com>
2026-05-13 14:58:16 +0100

    Correção Geral de erro

--- src/main/java/com/gestao/estacionamento/model/Utilizador.java
+++ src/main/java/com/gestao/estacionamento/model/Utilizador.java
@@ -1,12 +1,7 @@
 package com.gestao.estacionamento.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Data
@@ -17,8 +12,10 @@ public class Utilizador {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private String email;
-    private String senha;
+
+    private String username;
+
+    private String password;
+
     private String perfil;
 }
