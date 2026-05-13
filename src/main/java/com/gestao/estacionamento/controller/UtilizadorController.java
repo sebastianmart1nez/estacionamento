9f1b25f459f80e5002180afd455e4ebd418ae6a8
InesFaleiroR <insrsd@gmail.com>
2026-05-13 14:48:51 +0100

    correção de BUGS

--- src/main/java/com/gestao/estacionamento/controller/UtilizadorController.java
+++ src/main/java/com/gestao/estacionamento/controller/UtilizadorController.java
@@ -1,57 +1,68 @@
 package com.gestao.estacionamento.controller;
 
+import com.gestao.estacionamento.model.Utilizador;
 import com.gestao.estacionamento.service.UtilizadorService;
-import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UtilizadorController {
 
-    private final UtilizadorService utilizadorService;
+    @Autowired
+    private UtilizadorService service;
 
-    public UtilizadorController(UtilizadorService utilizadorService) {
-        this.utilizadorService = utilizadorService;
-    }
-
-    @GetMapping("/login")
-    public String mostrarLogin(){
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String fazerLogin(@RequestParam String email,
-                             @RequestParam String senha,
-                             HttpSession session,
-                             Model model){
-        Utilizador u = utilizadorService.autenticar(email,senha);
-        if (u != null){
-            session.setAttribute("utilizadorLogado", u);
-            return "redirect:/";
-        }
-
-        model.addAttribute("ERRO", "Email ou senha invalidos");
-        return "login";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpSession session){
-        session.invalidate();
-        return "redirect:/login";
-    }
-
+    // abrir página utilizadores
     @GetMapping("/utilizadores")
-    public String listarUtilizadores (Model model){
-        model.addAttribute("lista", utilizadorService.listarTodos());
+    public String abrirPaginaUtilizadores(Model model) {
+
+        model.addAttribute("utilizador", new Utilizador());
+
+        model.addAttribute("listaUtilizadores",
+                service.listarTodos());
+
         return "utilizadores";
     }
 
-    @PostMapping("/utilizadores")
-    public String adicionarUtilizador(Utilizador utilizador){
-        utilizadorService.guardar(utilizador);
+    // guardar utilizador
+    @PostMapping("/guardarUtilizador")
+    public String guardarUtilizador(
+            @ModelAttribute Utilizador utilizador) {
+
+        service.guardar(utilizador);
+
         return "redirect:/utilizadores";
+    }
+
+    // abrir login
+    @GetMapping("/login")
+    public String abrirLogin() {
+        return "login";
+    }
+
+    // validar login
+    @PostMapping("/login")
+    public String fazerLogin(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model) {
+
+        boolean loginValido =
+                service.validarLogin(username, password);
+
+        if (loginValido) {
+
+            model.addAttribute("mensagem",
+                    "Login efetuado com sucesso!");
+
+        } else {
+
+            model.addAttribute("erro",
+                    "Username ou password incorretos!");
+        }
+
+        return "login";
     }
 }
