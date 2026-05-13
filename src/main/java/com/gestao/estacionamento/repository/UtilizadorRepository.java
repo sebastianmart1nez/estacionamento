921050bc906cd057ab61fdbffa7ac94a94bc338b
InesFaleiroR <insrsd@gmail.com>
2026-05-13 14:54:31 +0100

    BUGS

--- src/main/java/com/gestao/estacionamento/repository/UtilizadorRepository.java
+++ src/main/java/com/gestao/estacionamento/repository/UtilizadorRepository.java
@@ -4,5 +4,6 @@ import com.gestao.estacionamento.model.Utilizador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UtilizadorRepository extends JpaRepository<Utilizador, Long> {
-    Utilizador findByEmailAndSenha(String email, String senha);
+
+    Utilizador findByUsername(String username);
 }
