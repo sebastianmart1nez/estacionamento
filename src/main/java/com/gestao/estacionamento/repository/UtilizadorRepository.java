a75d2eb32460179d1fa6f027ec22a0d8f5c4d628
João_Patrício <8452350@formacao.iefp.pt>
2026-05-13 14:31:04 +0100

    Criação do Utilizador no Repository.

---
+++ src/main/java/com/gestao/estacionamento/repository/UtilizadorRepository.java
@@ -0,0 +1,8 @@
+package com.gestao.estacionamento.repository;
+
+import com.gestao.estacionamento.model.Utilizador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtilizadorRepository extends JpaRepository<Utilizador, Long> {
+    Utilizador findByEmailAndSenha(String email, String senha);
+}
